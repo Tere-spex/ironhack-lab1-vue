8ece555226549264470863686eae734e9a2e54d4
tereh <terehdz.th@gmail.com>
2022-08-02 13:58:42 +0200

    helloworld()

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,15 +1,36 @@
 <template>
 <div>
-    <h1>This is a Home page</h1>
+    <h2>{{ message }}</h2>
+    <button @click="helloWorld">Message</button>
 </div>
 </template>
 
 <script>
 export default {
-
+  name: "message",
+  data(){
+    return{
+        message: "",
+    }
+  },
+  methods:{
+    helloWorld(){
+      this.message = "Hello World!!";
+    },
+  },
 }
 </script>
 
-<style>
-
+<style scoped>
+    h2{
+       color: blue;
+    }
+    button{
+        padding: 5px 30px;
+        background-color: rgb(20, 148, 107);
+        color: white;
+        font-size: 1.5rem;
+        margin: 15px;
+        border-radius: 25px;
+    }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <h1>This is a Home page</h1>
     <Message />
     <Counter :max="10"/>
   </div>
